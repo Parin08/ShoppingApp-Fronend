965a78af2d0307094bdf7de6fd73c69b96e92c04
parin <pate0468@algonquinlive.com>
2023-05-02 16:17:39 -0400

    Loginissue fix

--- src/Login.js
+++ src/Login.js
@@ -54,23 +54,9 @@ const Login = () => {
                     
             })
             .catch(err => console.log(err))
-            toast.success("Login successful.", {
-                position: 'top-center',
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-            });
+    
             navigate(0)
-            toast.success("Login successful.", {
-                position: 'top-center',
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-            });
+         
     
             
     }
